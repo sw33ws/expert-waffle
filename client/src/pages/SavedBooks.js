f13c617d0af0eceae44f14323d7317525247a4d8
Seb <gillisl22@outlook.com>
2022-07-26 15:24:32 -0400

    more bug fixs

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,21 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react'; // removed useEffect
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 import { useMutation, useQuery } from '@apollo/react-hooks';
 
-import { getMe, deleteBook } from '../utils/API';
+// import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 import { GET_ME } from '../utils/queries';
 import { REMOVE_BOOK } from '../utils/mutations';
 
 const SavedBooks = () => {
-  const { data } = useQuery(GET_ME);
+  const { data } = useQuery(GET_ME); // Maybe be needed
   const { removeBook } = useMutation(REMOVE_BOOK);
 
-  const [userData, setUserData] = useState({});
+  const [userData] = useState({}); // removed setUserData
 
   // use this to determine if `useEffect()` hook needs to run again
-  const user = data?.me || data?.userData || {};
+  // const user = data?.me || data?.userData || {};
   // const userDataLength = Object.keys(userData).length;
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -3,7 +3,7 @@ import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'reac
 import { useMutation } from '@apollo/react-hooks';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API'; // removed saveBook
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 import { SAVE_BOOK } from '../utils/mutations';
 
